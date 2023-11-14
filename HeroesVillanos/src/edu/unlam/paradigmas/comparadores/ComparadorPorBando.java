dfa600b67f38039d16b30ac0037b8597d05e8109
Ignacio Collazo <105324633+IgnaColla@users.noreply.github.com>
2023-11-13 23:52:35 -0300

    Create ComparadorPorBando.java

---
+++ HeroesVillanos/src/edu/unlam/paradigmas/comparadores/ComparadorPorBando.java
@@ -0,0 +1,21 @@
+package edu.unlam.paradigmas.comparadores;
+
+import java.util.Comparator;
+
+import edu.unlam.paradigmas.sistema.Competidor;
+
+public class ComparadorPorBando implements Comparator<Competidor> {
+    //Clase que me servirá para ordenar la lista de personajes
+    @Override
+    public int compare(Competidor c1, Competidor c2) {
+        // Comparar por tipo de bando
+        int comparacionBando = c1.getBando().compareTo(c2.getBando());
+
+        // Si tienen el mismo bando, comparar por nombre de personaje
+        if (comparacionBando == 0) {
+            return c1.getNombrePersonaje().compareTo(c2.getNombrePersonaje());
+        }
+
+        return comparacionBando;
+    }
+}
