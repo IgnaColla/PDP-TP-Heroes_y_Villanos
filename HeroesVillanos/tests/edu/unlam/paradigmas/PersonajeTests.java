ccd56e3efa7946fa3f13193548165c61cdb80c14
Gabriel Villa <40319809+gab-villa@users.noreply.github.com>
2023-11-17 02:30:43 -0100

    terminado personajes test

--- HeroesVillanos/tests/edu/unlam/paradigmas/PersonajeTests.java
+++ HeroesVillanos/tests/edu/unlam/paradigmas/PersonajeTests.java
@@ -2,6 +2,9 @@ package edu.unlam.paradigmas;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,24 +12,34 @@ import edu.unlam.paradigmas.excepciones.CaracteristicaExcepcion;
 import edu.unlam.paradigmas.sistema.Bandos;
 import edu.unlam.paradigmas.sistema.Caracteristica;
 import edu.unlam.paradigmas.sistema.Competidor;
+import edu.unlam.paradigmas.sistema.Liga;
+import edu.unlam.paradigmas.sistema.SistemaHeroesVillanos;
+import edu.unlam.paradigmas.sistema.UnidadCompetidor;
+import edu.unlam.paradigmas.sistema.Caracteristica.TipoCaracteristica;
 
 public class PersonajeTests {
-
-	private Competidor personajeUno;
-	private Caracteristica caracteristicasPersonajeUno;
-	private Competidor personajeDos;
-	private Caracteristica caracteristicasPersonajeDos;
-	private Competidor personajeTres;
-	private Caracteristica caracteristicasPersonajeTres;
+	SistemaHeroesVillanos sistema = new SistemaHeroesVillanos();
+	Caracteristica.TipoCaracteristica car;
+	
+	
+	Competidor personajeUno;
+	Caracteristica caracteristicasPersonajeUno;
+	Competidor personajeDos;
+	Caracteristica caracteristicasPersonajeDos;
+	Competidor personajeTres;
+	Caracteristica caracteristicasPersonajeTres;
 	@SuppressWarnings("unused")
-	private Competidor personajeCuatro;
-	private Caracteristica caracteristicasPersonajeCuatro;
+	Competidor personajeCuatro;
+	Caracteristica caracteristicasPersonajeCuatro;
 	@SuppressWarnings("unused")
-	private Competidor personajeCinco;
-	private Caracteristica caracteristicasPersonajeCinco;
+	Competidor personajeCinco;
+	Caracteristica caracteristicasPersonajeCinco;
 	@SuppressWarnings("unused")
-	private Competidor personajeSeis;
-	private Caracteristica caracteristicasPersonajeSeis;
+	Competidor personajeSeis;
+	Caracteristica caracteristicasPersonajeSeis;
+	Competidor v1, v2, v3, v4, v5, h1, h2, h3, h4, h5;
+	Liga ligaHeroes, ligaHeroes2, ligaHeroes3, ligaVillanos, ligaVillanos2, ligaVillanos3;
+	
 
 	@Before
 	public void setUp() throws Exception {
@@ -36,7 +49,41 @@ public class PersonajeTests {
 		caracteristicasPersonajeDos = new Caracteristica(20, 30, 40, 50);
 		personajeDos = new Competidor("Nombre real test p2", "Nombre personaje test p2", Bandos.Villano,
 				caracteristicasPersonajeDos);
-
+		
+		v1 = new Competidor("Ronan", "Ronan the Accuser", Bandos.Villano, new Caracteristica(150, 250, 180, 330));
+		// Héroe, Natasha Romanoff, Black Widow, 200, 150, 180, 200
+		h1 = new Competidor("Natasha Romanoff", "Black Widow", Bandos.Heroe, new Caracteristica(200, 150, 180, 250));
+		// Héroe, T'Challa, Black Panther, 200, 200, 200, 200
+		h2 = new Competidor("T'Challa", "Black Panther", Bandos.Heroe, new Caracteristica(200, 200, 200, 200));
+		// Villano, Loki, 150, 150, 150, 200
+		v2 = new Competidor("Loki", "Loki", Bandos.Villano, new Caracteristica(150, 150, 150, 200));
+		v3 = new Competidor("Joker", "Joker", Bandos.Villano, new Caracteristica(100, 200, 250, 200));
+		v4 = new Competidor("Watchmen", "Watchmen", Bandos.Villano, new Caracteristica(100, 150, 130, 200));
+		v5 = new Competidor("Magneto", "Magneto", Bandos.Villano, new Caracteristica(200, 175, 190, 225));
+		// Héroe, Steve Rogers, Captain America, 100, 200, 250, 200
+		h3 = new Competidor("Steve Rogers", "Captain America", Bandos.Heroe, new Caracteristica(100, 200, 250, 200));
+		h4 = new Competidor("Batman", "Batman", Bandos.Heroe, new Caracteristica(150, 200, 250, 200));
+		h5 = new Competidor("Superman", "Superman", Bandos.Heroe, new Caracteristica(100, 200, 250, 200));
+		
+		
+		ligaHeroes = new Liga();
+		ligaHeroes2 = new Liga();
+		ligaVillanos = new Liga();
+		
+		//agregarALiga
+		ligaHeroes.agregarALiga(h1);
+		ligaHeroes.agregarALiga(h2);
+		//ligaHeroes.setBando(Bandos.Heroe);
+		//ligaHeroes2.setBando(Bandos.Heroe);
+		
+		//System.out.println(ligaHeroes);
+		ligaVillanos.agregarALiga(v1);
+		ligaVillanos.agregarALiga(v2);
+		
+		//ligaDeLigas.agregarALiga(ligaHeroes);
+		//ligaDeLigas.agregarALiga(ligaHeroes2);
+		
+		//ligaDeLigas.add()
 	}
 
 	// esto no se tendría que probar acá, se tendría que probar en sistema?
@@ -82,112 +129,95 @@ public class PersonajeTests {
 				caracteristicasPersonajeSeis);
 	}
 
+	
+	/*
 	@Test
 	public void queFormatoLigaToStringSeRespete() {
         String primerResultadoEsperado = "Heroe, Nombre real test p1, Nombre personaje test p1, 10, 20, 30, 40";
         assertEquals(primerResultadoEsperado, personajeUno.toString());
         String segundoResultadoEsperado = "Villano, Nombre real test p2, Nombre personaje test p2, 20, 30, 40, 50";
         assertEquals(segundoResultadoEsperado, personajeDos.toString());
-	}
+	}*/
 	
+	
+	/*
+	 * public boolean personajePierdeContraUnidad(UnidadCompetidor u2, TipoCaracteristica caracteristica) {
+
+		int resultado = this.getValorCaracteristica(caracteristica) / this.contarIntegrantes()
+				- u2.getValorCaracteristica(caracteristica) / u2.contarIntegrantes();
+
+		if (resultado < 0) {
+			return true;
+		}
+
+		return false;
+	}
+	 * */
+	
+	
+	/*
+	 * personajeUno = new Competidor("Nombre real test p1", "Nombre personaje test p1", Bandos.Heroe,
+				caracteristicasPersonajeUno);
+		caracteristicasPersonajeDos = new Caracteristica(20, 30, 40, 50);
+		personajeDos = new Competidor("Nombre real test p2", "Nombre personaje test p2", Bandos.Villano,
+				caracteristicasPersonajeDos);
+	 * */
 	@Test
 	public void quePersonajePierdaContraOtroPersonajeEnBaseAVelocidad() { 
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(personajeDos, car.valueOf("VELOCIDAD")), true);
 	}
+	
 	
 	@Test
 	public void quePersonajePierdaContraOtroPersonajeEnBaseAFuerza() { 
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(personajeDos, car.valueOf("FUERZA")), true);
 	}
 	
 	@Test
 	public void quePersonajePierdaContraOtroPersonajeEnBaseAResistencia() { 
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(personajeDos, car.valueOf("RESISTENCIA")), true);
 		
 	}
 	
 	@Test
 	public void quePersonajePierdaContraOtroPersonajeEnBaseADestreza() {
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(personajeDos, car.valueOf("RESISTENCIA")), true);
 	}
+	
+	
 	
 	@Test
 	public void quePersonajePierdaContraOtraLigaEnBaseAVelocidad() {
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(ligaVillanos, car.valueOf("VELOCIDAD")), true);
 	}
 	
 	@Test
 	public void quePersonajePierdaContraOtraLigaEnBaseAFuerza() {
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(ligaVillanos, car.valueOf("FUERZA")), true);
 	}
 	
 	@Test
 	public void quePersonajePierdaContraOtraLigaEnBaseAResistencia() {
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(ligaVillanos, car.valueOf("RESISTENCIA")), true);
 	}
 	
 	@Test
 	public void quePersonajePierdaContraOtraLigaEnBaseADestreza() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariasLigasEnBaseAVelocidad() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariasLigasEnBaseAFuerza() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariasLigasEnBaseAResistencia() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariasLigasEnBaseADestreza() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariosPersonajeEnBaseAVelocidad() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariosPersonajeEnBaseAFuerza() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariosPersonajeEnBaseAResistencia() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariosPersonajeEnBaseADestreza() {
-
-	}
-	
-	@Test
-	public void quePersonajePierdaContraVariasPersonajeEnBaseADestreza() {
-
+		assertEquals(personajeUno.personajePierdeContraUnidad(ligaVillanos, car.valueOf("DESTREZA")), true);
 	}
 	
 	@Test
 	public void quePersonajeSeDistingaEnBaseAOtraUnidad() {
-
+		assertEquals(personajeUno.equals(personajeDos), false);
 	}
-	
+	/*
 	@Test
 	public void quePersonajeSeaIgualEnBaseAOtraUnidad() {
-
-	}
+		
+	}*/
 	
 	@Test
 	public void quePersonajeSepaContarse() {
-
-	}	
+		assertEquals(personajeUno.contarIntegrantes(), 1);
+	}
 }
